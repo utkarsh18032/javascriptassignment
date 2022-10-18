731a92cff9660a5d84636189c3817f2fc4c9d753
utkarsh18032 <utkarshpatil1834@gmail.com>
2022-10-18 15:16:24 +0530

    all files added

---
+++ function5.js
@@ -0,0 +1,10 @@
+function age()
+{
+    let a=document.getElementById('age').value
+    if(a>=18){
+        document.write("you are eligible for driving")
+    }
+    else{
+        document.write("you are not eligible")
+    }
+}
