7f9ef8a4825af8ac25317c6b6b829d270ce74df1
utkarsh18032 <utkarshpatil1834@gmail.com>
2022-10-18 10:32:46 +0530

    assg added

---
+++ function4.js
@@ -0,0 +1,13 @@
+/* Que.3 write a function pow (x,n) that returns x in power n. or, in other words 
+ multiplies x by itself n times and returns the result. */
+ //(means x is number and n is to the power)
+
+ function power(x,n)
+ {
+     return x ; x=x*3n
+ }
+ let powerReturn=(2,6);
+ console.log("power answere is :"+powerReturn)
+ 
+
+
